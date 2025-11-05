e713aac7426f7b20d35336acb61fefa30f952237
yoshizako <abcc.2401377@gmail.com>
2025-11-05 12:10:34 +0900

    header.php,footer.php,style.cssの作成
    新規ファイルの追加

---
+++ header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vanika - ASO Innovation</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body class="has-background-warning-light">
+    <nav class="navbar is-flex is-fixed-top is-justify-content-space-between is-align-items-center has-background-warning" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+            <a class="navbar-item" href="#">
+                <img src="logo-image/Vanikaロゴ.png">
+            </a>
+        </div>
+        <div class="navbar-end is-flex is-align-items-center is-flex-wrap-nowrap">
+            <a class="navbar-item" href="#">
+                <span class="icon">
+                    <i class="fas fa-search"></i>
+                </span>
+            </a>
+            <a class="navbar-item" href="#">
+                <span class="icon">
+                    <i class="fas fa-bell"></i>
+                </span>
+            </a>
+        </div>
+    </nav>
