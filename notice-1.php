a60c2e220bcf83f4c61c084a5e8d072fb30657af
yoshizako <abcc.2401377@gmail.com>
2025-11-10 12:44:19 +0900

    notice-1.php,notice-2.php,news-detail.php,notice-style.cssの作成
    新規ファイルの追加

---
+++ notice-1.php
@@ -0,0 +1,85 @@
+<?php
+$css_files = ['main-style.css', 'notice-style.css'];
+require 'header.php';
+?>
+<?php require 'header-menu.php'; ?>
+
+<div class="tabs is-fullwidth">
+  <ul>
+    <li class="is-active"><a href="notice-1.php">お知らせ</a></li>
+    <li><a href="notice-2.php">NEWS</a></li>
+  </ul>
+</div>
+
+<section class="section py-4">
+  <div class="container">
+    <div class="box notice-box is-flex is-justify-content-space-between is-align-items-center">
+      <div class="is-flex is-flex-direction-column">
+        <p>コメントが届きました。</p>
+        <p class="is-size-7 has-text-grey">2025-11-06 10:00:00</p>
+      </div>
+      <span class="icon is-medium has-text-grey">
+        <i class="fas fa-angle-right"></i>
+      </span>
+    </div>
+  </div>
+</section>
+
+<section class="section py-4">
+  <div class="container">
+    <div class="box notice-box is-flex is-justify-content-space-between is-align-items-center">
+      <div class="is-flex is-flex-direction-column">
+        <p>コメントが届きました。</p>
+        <p class="is-size-7 has-text-grey">2025-11-06 10:00:00</p>
+      </div>
+      <span class="icon is-medium has-text-grey">
+        <i class="fas fa-angle-right"></i>
+      </span>
+    </div>
+  </div>
+</section>
+
+<section class="section py-4">
+  <div class="container">
+    <div class="box notice-box is-flex is-justify-content-space-between is-align-items-center">
+      <div class="is-flex is-flex-direction-column">
+        <p>コメントが届きました。</p>
+        <p class="is-size-7 has-text-grey">2025-11-06 10:00:00</p>
+      </div>
+      <span class="icon is-medium has-text-grey">
+        <i class="fas fa-angle-right"></i>
+      </span>
+    </div>
+  </div>
+</section>
+
+<section class="section py-4">
+  <div class="container">
+    <div class="box notice-box is-flex is-justify-content-space-between is-align-items-center">
+      <div class="is-flex is-flex-direction-column">
+        <p>コメントが届きました。</p>
+        <p class="is-size-7 has-text-grey">2025-11-06 10:00:00</p>
+      </div>
+      <span class="icon is-medium has-text-grey">
+        <i class="fas fa-angle-right"></i>
+      </span>
+    </div>
+  </div>
+</section>
+
+<section class="section py-4">
+  <div class="container">
+    <div class="box notice-box is-flex is-justify-content-space-between is-align-items-center">
+      <div class="is-flex is-flex-direction-column">
+        <p>コメントが届きました。</p>
+        <p class="is-size-7 has-text-grey">2025-11-06 10:00:00</p>
+      </div>
+      <span class="icon is-medium has-text-grey">
+        <i class="fas fa-angle-right"></i>
+      </span>
+    </div>
+  </div>
+</section>
+
+<?php require 'footer-menu.php'; ?>
+<?php require 'footer.php'; ?>
