b3aaf8eedc954b7290d882c23fda8697821bdaa6
fuchi728 <fnnk1215@gmail.com>
2025-11-12 12:08:08 +0900

    mypage.php,logout.phpファイルの追加

---
+++ mypage.php
@@ -0,0 +1,62 @@
+<?php session_start(); ?>
+<?php
+$css_files = ['main-style.css', 'title.css', 'mypage.css'];
+require 'header.php';
+?>
+<?php require 'db-connect.php'; ?>
+
+<!--ページタイトル-->
+<div class="page_title">
+  <span class="title is-6">マイページ</span>
+</div>
+
+<div class="content">
+
+  <div class="box">
+    <div class="columns is-mobile is-vcentered">
+      <div class="column is-narrow p-0">
+        <div class="is-flex is-align-items-center">
+          <figure id="user_icon" class="image is-64x64 m-3">
+            <img class="is-rounded" src="user_icon/default.png" alt="ユーザー画像">
+          </figure>
+        </div>
+      </div>
+      <div class="column">
+        <p class="title is-5 m-0">ニックネーム</p>
+      </div>
+      <div class="column is-narrow">
+        <a href="#">編集</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="has-text-right">
+    フォロワー：
+    フォロー中：
+  </div>
+
+  <div class="block">
+    お名前：<br>
+    住所：<br>
+    自己紹介：
+
+  </div>
+
+  <!-- 閲覧履歴 -->
+  <div id="history" class="box">
+    <p class="title is-6">閲覧履歴</p>
+
+    <a class="button">...さらに表示</a>
+  </div>
+
+  <div class="has-text-right block">
+    <a id="help_link" href="help.php">お問い合わせはこちらから＞</a>
+  </div>
+
+  <form action="logout.php">
+    <button id="button" class="button is-fullwidth">ログアウト</button>
+  </form>
+</div>
+
+<?php require 'footer-menu.php'; ?>
+<?php require 'footer.php'; ?>
