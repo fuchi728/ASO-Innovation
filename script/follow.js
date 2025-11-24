c2eb5ba0b165a7a3c330431831b745a76ef8f752
fuchi728 <fnnk1215@gmail.com>
2025-11-24 16:06:46 +0900

    パス変更

--- script/follow.js
+++ script/follow.js
@@ -6,7 +6,7 @@ new Vue({
     },
     methods: {
         toggleFollow: function () {
-            fetch("../test/follow-insert.php", {
+            fetch("../ASO-Innovation/follow-insert.php", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ followed_id: this.followedId }),
