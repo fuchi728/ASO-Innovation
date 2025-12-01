c08c90b35d1ae5cfaf02d3526eb5a8d0602d8bb3
fuchi728 <fnnk1215@gmail.com>
2025-12-01 11:56:46 +0900

    更新ファイル

--- admin-news-detail.php
+++ admin-news-detail.php
@@ -1,6 +1,6 @@
 <?php require 'db-connect.php'; ?>
 <?php
-$page_title = '問い合わせ';
+$page_title = 'NEWS発信';
 $css_files = ['main-style.css', 'admin-header.css', 'admin-news-detail-style.css'];
 require 'admin-header.php';
 ?>
