baf7970a82f6072e94432ddd7c25abd5552a2701
fuchi728 <fnnk1215@gmail.com>
2025-12-01 16:08:48 +0900

    リダイレクト処理訂正

--- header.php
+++ header.php
@@ -1,10 +1,6 @@
-<?php
-if (!isset($css_files) || !is_array($css_files)) {
-    $css_files = [];
-}
-?>
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,8 +8,14 @@ if (!isset($css_files) || !is_array($css_files)) {
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
-    <?php foreach ($css_files as $css): ?>
-        <link rel="stylesheet" href="css/<?= htmlspecialchars($css, ENT_QUOTES) ?>">
-    <?php endforeach; ?>
+    <?php
+    if (!isset($css_files) || !is_array($css_files)) {
+        $css_files = [];
+    }
+    foreach ($css_files as $css) {
+        echo '<link rel="stylesheet" href="css/' . htmlspecialchars($css, ENT_QUOTES) . '">' . "\n";
+    }
+    ?>
 </head>
+
 <body>
